0e492f4eeefd6f0eececda2c13e6c374e6769b22
Thiago Peres <180028278@aluno.unb.br>
2024-07-12 17:47:02 -0300

    Delete blog directory

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from blog import views
-
-urlpatterns = [
-	
-	path("dashboard/", views.dashboard, name="dashboard"),
-	path("blog/", views.blog, name="blog"),
-	path("comentario/", views.comentario, name="comentario"),
-]
