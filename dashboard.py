4f2f2659546d1f3cd87e6f0a9411174d9837ab9e
Thiago Peres <180028278@aluno.unb.br>
2024-07-25 10:23:11 -0300

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+st.set_page_config(
+    page_title="Dashboard",
+    layout="wide",
+    page_icon=":bar_chart: "
+)
+
+df = pd.read_excel("DB_FIRE.xlsx")
+    
+st.sidebar.header("Filtros")
+ano = st.sidebar.slider(
+    "Selecione o ano",
+    options=df["Ano"].unique(),
+    default=df["Ano"].unique()
+)
+mes = st.sidebar.multiselect(
+    "Selecione o mes",
+    options=df["Mês"].unique(),
+    default=df["Mês"].unique()
+)
+
+df_selection = df.query(
+    "Ano == @ano & Mês == @mes"
+)
+df = df_selection
+with st.expander("Grafico"):
+    st.line_chart(df, x="Ano", y="Mês")
+
+st.dataframe(df_selection)
+
