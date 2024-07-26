85854700b2979ffc5f4db9f8c9c93c94de1f3e84
Thiago Peres <180028278@aluno.unb.br>
2024-07-26 09:53:08 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,9 +10,9 @@ st.set_page_config(
 df = pd.read_excel("DB_FIRE.xlsx")
 st.sidebar.header("Filtros")
 anos = st.sidebar.selectbox("Selecione o ano:", df["Ano"].unique())
-
+mes = st.sidebar.selectbox("Selecione o mês", df["Mês"].unique())
 df_selection = df.query(
-    "Ano == @anos"
+    "Ano == @anos & Mês == @mes"
 )
 df = df_selection
 
