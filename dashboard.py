89b6959ca3f9bfd460a4695f9b1bdc02db27ead9
Thiago Peres <180028278@aluno.unb.br>
2024-07-26 09:24:19 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,9 +11,12 @@ anos = [2020, 2021, 2022, 2023, 2024]
 st.sidebar.header("Filtros")
 year = st.sidebar.selectbox("Selecione o ano:", anos)
 
-
+df_selection = df.query(
+    "year == ano"
+)
+df = df_selection
 with st.expander("Grafico"):
     st.line_chart(df, x="Ano", y="Mês")
 
-st.dataframe(df)
+st.dataframe(df_selection)
 
