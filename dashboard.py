abcc6abf64c3255911b0b35fb5e229650bac9a5c
Thiago Peres <180028278@aluno.unb.br>
2024-07-26 09:22:42 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,23 +12,8 @@ st.sidebar.header("Filtros")
 year = st.sidebar.selectbox("Selecione o ano:", anos)
 
 
-ano = st.sidebar.slider(
-    "Selecione o ano",
-    options=df["Ano"].unique(),
-    default=df["Ano"].unique()
-)
-mes = st.sidebar.multiselect(
-    "Selecione o mes",
-    options=df["Mês"].unique(),
-    default=df["Mês"].unique()
-)
-
-df_selection = df.query(
-    "Ano == @ano & Mês == @mes & Anos == @anos"
-)
-df = df_selection
 with st.expander("Grafico"):
     st.line_chart(df, x="Ano", y="Mês")
 
-st.dataframe(df_selection)
+st.dataframe(df)
 
