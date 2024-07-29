62be77027878f8f3920499b4d913325b4dc24a08
Thiago Peres <180028278@aluno.unb.br>
2024-07-29 10:23:21 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,14 +9,6 @@ st.set_page_config(layout="wide")
 m = leafmap.Map(center=[40, -100], zoom=4)
 cities = "https://raw.githubusercontent.com/ThCP00/PI/main/DB_FIRE_MIN.csv"
 
-m.add_points_from_xy(
-    cities,
-    x="longitude",
-    y="latitude",
-    icon_names=["gear", "map", "leaf", "globe"],
-    spin=True,
-    add_legend=True,
-    )
 
 m.add_heatmap(
     cities,
