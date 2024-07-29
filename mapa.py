5807d158a16b51c1c114dc4ac5006bb5b2aa5993
Thiago Peres <180028278@aluno.unb.br>
2024-07-29 10:36:22 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -4,7 +4,10 @@ import leafmap.foliumap as leafmap
 
 
 st.set_page_config(layout="wide")
-
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['latitude', 'longitude'])
+st.mapO(df)
 
 m = leafmap.Map(center=[40, -100], zoom=4)
 cities = "https://raw.githubusercontent.com/ThCP00/PI/main/DB_FIRE_MIN.csv"
