e5eaaa9aed8d5352540891356be8c09ff6d360ed
Thiago Peres <180028278@aluno.unb.br>
2024-07-29 10:56:07 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -4,16 +4,11 @@ import leafmap.foliumap as leafmap
 
 
 st.set_page_config(layout="wide")
-m = leafmap.Map(center=[40, -100], zoom=4)
-cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
 
-m.add_points_from_xy(
-    cities,
-    x="longitude",
-    y="latitude",
-    icon_names=["gear", "map", "leaf", "globe"],
-    spin=True,
-    add_legend=True,
-    )
+m = leafmap.Map()
+data = "https://raw.githubusercontent.com/ThCP00/PI/main/DB_FIRE_MIN.csv"
+df = pd.read_csv(data)
+m.add_points_from_xy(df, x="longitude", y="latitude")
+m
 
 m.to_streamlit(height=700)
