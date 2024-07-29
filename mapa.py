15a33ce8fe4e29b4e4bbc24578550b180bf66924
Thiago Peres <180028278@aluno.unb.br>
2024-07-29 08:59:15 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -4,19 +4,13 @@ import leafmap.foliumap as leafmap
 
 st.title("Mapa")
 
-col1, col2 = st.columns([4, 1])
-options = list(leafmap.basemaps.keys())
-index = options.index("OpenTopoMap")
-
-with col2:
-
-    basemap = st.selectbox("Select a basemap:", options, index)
-
-
-with col1:
-
-    m = leafmap.Map(
-        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
-    )
-    m.add_basemap(basemap)
-    m.to_streamlit(height=780, width = 1700)
+filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+m = leafmap.Map(center=[40, -100], zoom=4)
+m.add_heatmap(
+    filepath,
+    latitude="latitude",
+    longitude="longitude",
+    value="pop_max",
+    name="Heat map",
+    radius=20,
+)
