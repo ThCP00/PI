745663620145ba05224d9f0742d873f052896975
Thiago Peres <180028278@aluno.unb.br>
2024-07-29 09:12:01 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -5,10 +5,6 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-
-m = leafmap.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
-m.to_streamlit(height=700)
 with st.expander("See source code"):
     with st.echo():
 
