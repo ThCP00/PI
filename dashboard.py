e19680d87ddc870d2d35b24b0cdaff996541ae01
Thiago Peres <180028278@aluno.unb.br>
2024-08-07 10:00:07 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -35,7 +35,24 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
         ) 
     # height=300
     return heatmap
+def make_choropleth(input_df, input_id, input_column, input_color_theme):
+    choropleth = px.choropleth(input_df, locations=input_id, color=input_column, locationmode="USA-states",
+                               color_continuous_scale=input_color_theme,
+                               range_color=(0, max(df_selected_year.population)),
+                               scope="usa",
+                               labels={'population':'Population'}
+                              )
+    choropleth.update_layout(
+        template='plotly_dark',
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)',
+        margin=dict(l=0, r=0, t=0, b=0),
+        height=350
+    )
+    return choropleth
 
+choropleth = make_choropleth(df_selection, 'states_code', 'population', selected_color_theme)
+st.plotly_chart(choropleth, use_container_width=True)
 
 heatmap = make_heatmap(df, 'Ano', 'Mês', 'Dia', selected_color_theme)
 st.altair_chart(heatmap, use_container_width=True)
