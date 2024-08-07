6432499831996c3f709069f473d0cad98f1a6dfc
Thiago Peres <180028278@aluno.unb.br>
2024-08-07 10:16:14 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -36,7 +36,6 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
     # height=300
     return heatmap
 def make_choropleth(input_df, input_id, input_column, input_color_theme):
-    locations = ["Brazil"],
     choropleth = px.choropleth(input_df, locations = input_id, color=input_column, locationmode="country names",
                                color_continuous_scale=input_color_theme,
                                range_color=(0, max(df_selection.Ano)),
