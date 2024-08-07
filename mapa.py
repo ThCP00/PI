9ba8e46e680f5bb90bd903236bab661c0dbf6dfc
Thiago Peres <180028278@aluno.unb.br>
2024-08-07 08:53:19 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 
 options = list(leafmap.basemaps.keys()) 
 index = options.index("OpenTopoMap")
-basemap = st.selectbox("Select a basemap:", options, index)
+basemap = st.selectbox("Selecione o tipo de mapa:", options, index)
 
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DB_FIRE_MIN.csv"
