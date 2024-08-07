280dbf56fddd6f4a63da2f4e715fa53db33ead73
Thiago Peres <180028278@aluno.unb.br>
2024-08-07 08:38:37 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -6,10 +6,14 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
+options = list(leafmap.basemaps.keys()) 
+basemap = st.selectbox("Select a basemap:", options, index)
 
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DB_FIRE_MIN.csv"
 df = pd.read_csv(data)
 m.add_points_from_xy(df, x="longitude", y="latitude")
+
+m.add_basemap(basemap)
 
 m.to_streamlit(height=700)
