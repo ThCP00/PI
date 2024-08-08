af712057e8631509fcd1438d6a866d37165bc826
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 09:50:21 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import leafmap.foliumap as leafmap
-
+import geopandas as gpd
 
 
 st.set_page_config(layout="wide")
@@ -12,3 +12,11 @@ df = pd.read_csv(data)
 m.add_points_from_xy(df, x="longitude", y="latitude")
 
 m.to_streamlit(height=600)
+
+
+df = gpd.read_file('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/geojs-DF.json')
+df['lon'] = df.geometry.x  # extract longitude from geometry
+df['lat'] = df.geometry.y  # extract latitude from geometry
+df = df[['lon','lat']]     # only keep longitude and latitude
+st.write(df.head())        # show on table for testing only
+st.map(df)                 # show on map
