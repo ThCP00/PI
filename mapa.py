7bcb3ebf9930f1cc24c019c993cd97997da28814
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 09:22:10 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,6 +9,4 @@ m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/ADM_DF.csv"
 df = pd.read_csv(data)
 
-m.add_basemap(basemap)
-
 m.to_streamlit(height=700)
