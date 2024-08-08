3cb62fc044f77281e5c6cbf932499bb62cb25d06
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 10:26:09 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -13,7 +13,6 @@ basemap = st.selectbox("Selecione o tipo de mapa:", options, index)
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/geomap.csv"
 df = pd.read_csv(data)
-m.add_points_from_xy(df, x="longitude", y="latitude")
 
 m.add_basemap(basemap)
 
