24fd97641d70fb05c899edd48c121aa62219e9be
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 09:54:32 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -6,13 +6,6 @@ import geopandas as gpd
 
 st.set_page_config(layout="wide")
 
-m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
-data = "https://raw.githubusercontent.com/ThCP00/PI/main/DB_FIRE_MIN.csv"
-df = pd.read_csv(data)
-m.add_points_from_xy(df, x="longitude", y="latitude")
-
-m.to_streamlit(height=600)
-
 
 df = gpd.read_file('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/geojs-DF.json')
 df['lon'] = df.geometry.x  # extract longitude from geometry
