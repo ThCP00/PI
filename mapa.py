ae539729a0553ee75c9fdfaf7f2372ded20ea51d
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 09:57:57 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -8,8 +8,4 @@ st.set_page_config(layout="wide")
 
 
 df = gpd.read_file('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/geojs-DF.json')
-df['lon'] = df.geometry.x  # extract longitude from geometry
-df['lat'] = df.geometry.y  # extract latitude from geometry
-df = df[['lon','lat']]     # only keep longitude and latitude
-st.write(df.head())        # show on table for testing only
 st.map(df)                 # show on map
