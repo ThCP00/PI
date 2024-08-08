06b16109381918bf15bb034db30f74767b2749f6
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 09:23:52 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -5,8 +5,10 @@ import leafmap.foliumap as leafmap
 
 
 st.set_page_config(layout="wide")
+
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DB_FIRE_MIN.csv"
 df = pd.read_csv(data)
+m.add_points_from_xy(df, x="longitude", y="latitude")
 
 m.to_streamlit(height=700)
