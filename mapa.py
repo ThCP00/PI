dbf5be109d6d63e2d612ac719eecd9df592f0b12
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 14:41:04 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import leafmap.foliumap as leafmap
 import geopandas as gpd
-
+from utils.plots import line_plot, bar_plot
+from utils.style import set_background
 
 st.set_page_config(layout="wide")
 
@@ -17,3 +18,7 @@ m.add_points_from_xy(df, x="longitude", y="latitude", layer_name="ra_nome")
 m.add_basemap(basemap)
 
 m.to_streamlit(height=700)
+
+d= pd.read_csv('Data/inmet_inpe.csv')
+st.plotly_chart(line_plot(d, 'Data Medição', 'Frequência de incêndios',
+                    'Frequência de incêndios em Brasília - 1998 a 2022', 'red'), use_container_width=True)
