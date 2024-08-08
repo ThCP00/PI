3e0f37484d994ac0764daad8062d77a0c00adcfd
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 09:58:31 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -8,4 +8,4 @@ st.set_page_config(layout="wide")
 
 
 df = gpd.read_file('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/geojs-DF.json')
-st.map(df)                 # show on map
+st.map(df) 
