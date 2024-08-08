f76f14c7026a295bb23e7e5ecb8760cd7cc96f2b
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 14:09:19 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -13,7 +13,7 @@ basemap = st.selectbox("Selecione o tipo de mapa:", options, index)
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/ADM_DF.csv"
 df = pd.read_csv(data)
-m.add_points_from_xy(df, x="longitude", y="latitude")
+m.add_points_from_xy(df, x="longitude", y="latitude". layer_name="ra_nome")
 m.add_basemap(basemap)
 
 m.to_streamlit(height=700)
