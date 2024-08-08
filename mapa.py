ec52335348ef7d7b4ff0ecf3781fbdbb4f53e7dd
Thiago Peres <180028278@aluno.unb.br>
2024-08-08 09:21:34 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -5,11 +5,6 @@ import leafmap.foliumap as leafmap
 
 
 st.set_page_config(layout="wide")
-
-options = list(leafmap.basemaps.keys()) 
-index = options.index("OpenTopoMap")
-basemap = st.selectbox("Selecione o tipo de mapa:", options, index)
-
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/ADM_DF.csv"
 df = pd.read_csv(data)
