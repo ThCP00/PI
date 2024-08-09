4b9b8e840e3fe38de406701fab49f76d4b1c37bc
Thiago Peres <180028278@aluno.unb.br>
2024-08-09 16:31:04 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -18,4 +18,4 @@ m.add_basemap(basemap)
 m.to_streamlit(height=700)
 
 df= pd.read_csv('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/inmet_inpe.csv')
-st.bar_chart(df, x='frequencia_incendios', y='ano')
+st.bar_chart(df, x='ano', y='frequencia_incendios')
