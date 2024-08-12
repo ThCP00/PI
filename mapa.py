56cece99fbca570fb68b14245a082aab36c52813
Thiago Peres <180028278@aluno.unb.br>
2024-08-12 09:40:03 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,7 +9,7 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 df = gpd.read_file(data)
-df = df[['longitude','latitude']] 
+df = df[['DataHora','longitude','latitude']] 
 m.add_points_from_xy(df, x="longitude", y="latitude")
 
 
