9fd46c09024d150cec6c6d00aff5a88145da720f
Thiago Peres <180028278@aluno.unb.br>
2024-08-12 09:25:06 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -10,6 +10,5 @@ m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 df = gpd.read_file(data)
 m.add_points_from_xy(df, x="longitude", y="latitude")
-
 m.to_streamlit(height=700)
 
