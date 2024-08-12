e159e1cf317bda7040a3744ba72e0f58d81a954c
Thiago Peres <180028278@aluno.unb.br>
2024-08-12 11:04:13 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import leafmap.foliumap as leafmap
 import geopandas as gpd
-
+import geobr
 st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 
-
+gdf= geobr.read_municipality(code_muni='DF', year=2020)
+gdf.head()
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 df = gpd.read_file(data)
