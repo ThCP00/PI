bf37a61b7ba71d6dfaff906c2fcd2b803b42620c
Thiago Peres <180028278@aluno.unb.br>
2024-08-12 09:11:14 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -2,14 +2,20 @@ import streamlit as st
 import pandas as pd
 import leafmap.foliumap as leafmap
 import geopandas as gpd
+import basedosdados as bd
 
+# Para carregar o dado direto no pandas
+df = bd.read_table(dataset_id='br_inpe_queimadas',
+table_id='microdados',
+billing_project_id="<YOUR_PROJECT_ID>")
+st.dataframe(df)
 st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 
 
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
-df = pd.read_csv(data)
-m.add_points_from_xy(df, x="longitude", y="latitude", layer_name="ra_nome")
+dft = pd.read_csv(data)
+m.add_points_from_xy(dft, x="longitude", y="latitude", layer_name="ra_nome")
 
 m.to_streamlit(height=700)
 
