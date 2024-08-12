9692aee341fc2b0b3ab5cfee72e51115deddcef3
Thiago Peres <180028278@aluno.unb.br>
2024-08-12 09:36:26 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,9 +9,8 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/Area_de_Protecao_Ambiental.csv"
 df = gpd.read_file(data)
-df['lon'] = df.['geometry'].x
-df['lat'] = df.['geometry'].y
-df = df[['lon','lat']]
+df['lon'] = df.st_area.x
+df['lat'] = df.st_area.y
 st.map(df)
 m.add_points_from_xy(df, x="lon", y="lat")
 m.to_streamlit(height=700)
