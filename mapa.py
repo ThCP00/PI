b53acc90d676a4cfcbce768bb055e1b4132f352c
Thiago Peres <180028278@aluno.unb.br>
2024-08-12 09:13:47 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -2,13 +2,7 @@ import streamlit as st
 import pandas as pd
 import leafmap.foliumap as leafmap
 import geopandas as gpd
-import basedosdados as bd
 
-# Para carregar o dado direto no pandas
-df = bd.read_table(dataset_id='br_inpe_queimadas',
-table_id='microdados',
-billing_project_id="<YOUR_PROJECT_ID>")
-st.dataframe(df)
 st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 
 
