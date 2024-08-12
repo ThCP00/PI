1e9e667b3ae6d20597990f7b83e07fa438e123de
Thiago Peres <180028278@aluno.unb.br>
2024-08-12 09:17:50 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,7 +9,7 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 dft = pd.read_csv(data)
-m.add_points_from_xy(dft, x="longitude", y="latitude",)
+m.add_points_from_xy(dft, x="longitude", y="latitude")
 
 m.to_streamlit(height=700)
 
