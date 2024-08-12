1e767fbb5364493017236124526389d06a0dc33d
Thiago Peres <180028278@aluno.unb.br>
2024-08-12 10:24:24 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -62,7 +62,7 @@ st.altair_chart(heatmap, use_container_width=True)
 
 df= pd.read_csv('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/inmet_inpe.csv')
 st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
-st.bar_chart(df, x='mes', y=sum.('frequencia_incendios'), x_label='Mês', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
+st.bar_chart(df, x='mes', y=sum('frequencia_incendios'), x_label='Mês', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
 st.bar_chart(df.groupby('mes_numero', sort=False).sum('frequencia_incendios'))
 
 
