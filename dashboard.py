c8193f8bf67a7df49e9ece2c4b1edd214164f64c
Thiago Peres <180028278@aluno.unb.br>
2024-08-13 10:37:38 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,4 @@ df= pd.read_csv('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/inmet_in
 st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
 st.bar_chart(df, x='mes', y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
 st.bar_chart(df.groupby('mes_numero', sort=False).sum('frequencia_incendios'))
-
-
-st.dataframe(df_selection)
 st.dataframe(df)
