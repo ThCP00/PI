0fb4f518361a631e4b7028db539f97ae152ef419
Thiago Peres <180028278@aluno.unb.br>
2024-08-13 10:36:35 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,18 +7,6 @@ st.set_page_config(
     page_title="Dashboard",
     layout="wide",
 )
-df = pd.read_excel("https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE.xlsx")
-
-with st.sidebar:
-    st.sidebar.header("Filtros")
-    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
-    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
-    anos = st.sidebar.selectbox("Selecione o ano:", df["Ano"].unique())
-    df_selection = df.query(
-        "Ano == @anos"
-    )
-
-
 df= pd.read_csv('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/inmet_inpe.csv')
 st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
 st.bar_chart(df, x='mes', y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
