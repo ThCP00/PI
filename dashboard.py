75537ae67b9cc2524a70fd5764a08e1147caf2d4
Thiago Peres <180028278@aluno.unb.br>
2024-08-13 11:01:11 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -61,7 +61,7 @@ st.altair_chart(heatmap, use_container_width=True)
 df= pd.read_csv('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/inmet_inpe.csv')
 anos = st.sidebar.selectbox("Selecione o ano:", df["ano"].unique())
 df_selection = df.query(
-        "ano == @anos"
+        "ano == @anos")
 st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
 st.bar_chart(df, x='mes', y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="#ffffff", horizontal=False, stack='layered')
 st.bar_chart(df.groupby('mes_numero', sort=False).sum('frequencia_incendios'))
