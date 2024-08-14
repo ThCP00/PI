e30c0e39dc11ada197d39d647097d30019eed7bb
Thiago Peres <180028278@aluno.unb.br>
2024-08-14 09:08:46 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -4,8 +4,7 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 
-gdf= geobr.read_municipality(code_muni='DF', year=2020)
-gdf.head()
+
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 regiao = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/ADM_DF.csv"
