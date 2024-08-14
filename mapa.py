647b2ca80aa3d85a0ca42defd08ccd43f89cb148
Thiago Peres <180028278@aluno.unb.br>
2024-08-14 11:09:37 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -8,9 +8,10 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 regiao ="DADOS/DF_Municipios_2022.shp"
+adm = "DADOS/sdia_ra_2022.shp"
 
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
-m.add_shp(regiao)
+m.add_shp(adm)
 df = gpd.read_file(data)
 df = df[['DataHora','longitude','latitude','Satelite']]
 m.add_points_from_xy(df,
