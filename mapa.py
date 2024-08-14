5870634e6766becd0d33cf5766ba93bd1c7f7ab0
Thiago Peres <180028278@aluno.unb.br>
2024-08-14 10:20:52 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -8,5 +8,5 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data ="DADOS/DF_RG_Intermediarias_2022.shp"
-m.add_shp(data)
+m.add_shp("DADOS/DF_RG_Intermediarias_2022.shp")
 m.to_streamlit(height=700)
