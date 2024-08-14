9c9e4f62a1294bc3d64d2265e8c516b6e4239460
Thiago Peres <180028278@aluno.unb.br>
2024-08-14 10:40:49 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -3,9 +3,13 @@ import pandas as pd
 import leafmap.leafmap as leafmap
 import geopandas as gpd
 import matplotlib.pyplot as plt
+
 st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
+
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
+
 regiao ="DADOS/DF_Municipios_2022.shp"
+
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 m.add_heatmap(data,
               latitude="latitude",
@@ -19,4 +23,4 @@ df = df[['DataHora','longitude','latitude','Satelite']]
 m.add_points_from_xy(df,
                      x="longitude",
                      y="latitude")
-
+m.to_streamlit(height=700)
