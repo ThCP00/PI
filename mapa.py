41c3c2eb4e45fea5ab586795c9bd3164abec80b0
Thiago Peres <180028278@aluno.unb.br>
2024-08-14 10:04:47 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -7,6 +7,6 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 
 
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
-data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DF_Municipios_2022.shp"
+data = gpd.read_file("https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DF_Municipios_2022.shp")
 m.add_shp(data)
 m.to_streamlit(height=700)
