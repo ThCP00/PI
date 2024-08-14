66a20850ff46eac39fead1e5d24ab641eaf1392c
Thiago Peres <180028278@aluno.unb.br>
2024-08-14 09:00:59 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -12,7 +12,7 @@ data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 regiao = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/ADM_DF.csv"
 df = gpd.read_file(data)
 df = df[['DataHora','longitude','latitude','Satelite']]
-m.add_geojson(regions, layer_name="Regiões administrativas")
+m.add_json(regions, layer_name="Regiões administrativas")
 m.add_points_from_xy(df,
                      x="longitude",
                      y="latitude",
