3067085ceca4ae4633393e3f7f123f831751c7c6
Thiago Peres <180028278@aluno.unb.br>
2024-08-14 08:52:03 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,10 +9,13 @@ gdf= geobr.read_municipality(code_muni='DF', year=2020)
 gdf.head()
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
+regiao = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/ADM_DF.csv"
 df = gpd.read_file(data)
-df = df[['DataHora','longitude','latitude','Satelite']] 
-m.add_points_from_xy(df, x="longitude", y="latitude")
-
-
+df = df[['DataHora','longitude','latitude','Satelite']]
+m.add_geojson(regions, layer_name="Regiões administrativas")
+m.add_points_from_xy(df,
+                     x="longitude",
+                     y="latitude",
+                    color_column="regiao")
 m.to_streamlit(height=700)
 
