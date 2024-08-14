480c9ffc4537f6553428ccb6101be66d53d25ca5
Thiago Peres <180028278@aluno.unb.br>
2024-08-14 09:09:08 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -8,7 +8,7 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 regiao = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/ADM_DF.csv"
-df = pd.read_file(data)
+df = pd.read_csv(data)
 
 m.add_heatmap(df,
              latitude="latitude",
