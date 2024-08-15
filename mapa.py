a975c036f07990fb4650044b5edbbdead0f3310f
Thiago Peres <180028278@aluno.unb.br>
2024-08-15 09:37:13 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,12 +9,12 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 adm = "DADOS/sdia_ra_2022.shp"
 tab1, tab2 = st.tabs(["Pontos","Calor"])
-m = leafmap.Map(center=[-15.7, -47.7], zoom=10,basemap='SATELLITE')
-
-m.add_shp(adm)
-df = gpd.read_file(data)
-df = df[['DataHora','longitude','latitude','Satelite']]
-m.add_points_from_xy(df,
+m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
+with tab1:
+  df = gpd.read_file(data)
+  df = df[['DataHora','longitude','latitude','Satelite']]
+  m.add_points_from_xy(df,
                      x="longitude",
                      y="latitude")
+m.add_shp(adm)
 m.to_streamlit(height=800)
