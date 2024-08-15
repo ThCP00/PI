0d84783f1702f29a86c2b62fe72c7d0e60cdfb79
Thiago Peres <180028278@aluno.unb.br>
2024-08-15 09:44:04 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -11,11 +11,7 @@ adm = "DADOS/sdia_ra_2022.shp"
 tab1, tab2 = st.tabs(["Pontos","Calor"])
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 with tab1:
-  df = gpd.read_file(data)
-  df = df[['DataHora','longitude','latitude','Satelite']]
-  m.add_points_from_xy(df,
-                     x="longitude",
-                     y="latitude")
+
 with tab2:
   df = gpd.read_file(data)
   df = df[['DataHora','longitude','latitude','Satelite']]
