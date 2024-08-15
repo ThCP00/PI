a4505c41b1848e77d32fd7c7c207b270133e6b4b
Thiago Peres <180028278@aluno.unb.br>
2024-08-15 08:46:36 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,7 +9,12 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 adm = "DADOS/sdia_ra_2022.shp"
 
+df = gpd.read_file(data)
+df = df[['DataHora','longitude','latitude','Satelite']]
+
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
+m.add_shp(adm)
+
 left_layer=folium.WmsTileLayer(
     url=data,
     layers="ROADMAP",
@@ -17,9 +22,9 @@ left_layer=folium.WmsTileLayer(
     m.add_points_from_xy(df,
                      x="longitude",
                      y="latitude"))
-m.add_shp(adm)
-df = gpd.read_file(data)
-df = df[['DataHora','longitude','latitude','Satelite']]
+
+
+
 
 m.split_map(left_layer, right_layer='SATELLITE')
 m.to_streamlit(height=800)
