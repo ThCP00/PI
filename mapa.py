f64d16cd517d9d305d3c57f1e6593b3facf7145e
Thiago Peres <180028278@aluno.unb.br>
2024-08-15 10:42:27 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -8,6 +8,7 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 
 data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 adm = "DADOS/sdia_ra_2022.shp"
+area_protec = "DADOS/Area_de_Protecao_Ambiental.shp"
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 df = gpd.read_file(data)
 df = df[['DataHora','longitude','latitude','Satelite']]
@@ -15,5 +16,6 @@ m.add_points_from_xy(df,
                      x="longitude",
                      y="latitude")
 m.split_map(left_layer='ROADMAP', right_layer='HYBRID')
+m.add_shp(area_protec)
 m.add_shp(adm)
 m.to_streamlit(height=800)
