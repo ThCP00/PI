20eb694843821c2dcd68f27bf87b58d8ba5a5561
Thiago Peres <180028278@aluno.unb.br>
2024-08-15 11:05:46 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -11,9 +11,7 @@ adm = "DADOS/sdia_ra_2022.shp"
 area_protec = "DADOS/Area_de_Protecao_Ambiental.shp"
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 df = gpd.read_file(data)
-m.add_points_from_xy(df,
-                     x="longitude",
-                     y="latitude")
+m.add_points_from_xy(df)
 m.split_map(left_layer='ROADMAP', right_layer='HYBRID')
 m.add_shp(adm)
 m.to_streamlit(height=800)
