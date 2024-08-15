e391f762117560912dce9b4e710b41b3625e5c34
Thiago Peres <180028278@aluno.unb.br>
2024-08-15 09:44:50 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -10,14 +10,11 @@ data = "https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/DB_FIRE_MIN.csv"
 adm = "DADOS/sdia_ra_2022.shp"
 tab1, tab2 = st.tabs(["Pontos","Calor"])
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
-
-with tab2:
-  df = gpd.read_file(data)
-  df = df[['DataHora','longitude','latitude','Satelite']]
-  m.add_heatmap(df,
-               x="longitude",
-               y="latitude",
-               value="DataHora")
+df = gpd.read_file(data)
+df = df[['DataHora','longitude','latitude','Satelite']]
+m.add_points_from_xy(df,
+                     x="longitude",
+                     y="latitude")
 
 m.add_shp(adm)
 m.to_streamlit(height=800)
