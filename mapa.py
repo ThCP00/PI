0f5f7a7c745741ab35afc35f486fd79e3e027311
Thiago Peres <180028278@aluno.unb.br>
2024-08-19 16:47:29 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -9,6 +9,8 @@ st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
 data = "DADOS/INPE_20_24.csv"
 adm = "DADOS/sdia_ra_2022.shp"
 area_protec = "DADOS/Area_de_Protecao_Ambiental.shp"
+st.markdown("<h2 style='text-align: center; color: white;'> Visualização das séries históricas </h2>",
+            unsafe_allow_html=True)
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
 df = pd.read_csv(data)
 m.add_points_from_xy(df,
