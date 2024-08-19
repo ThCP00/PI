5f0744c559da991f976e5d62c8aa95ffa3a6a691
Thiago Peres <180028278@aluno.unb.br>
2024-08-19 19:48:32 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,10 +17,10 @@ df= pd.read_csv('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/inmet_in
 anos = st.sidebar.selectbox("Selecione o ano:", df["ano"].unique())
 df_selection = df.query(
         "ano == @anos")
-
+test=px.bar(df_selection, x="mes", y="frequencia_incendios")
+test.show()
 with tab1:
     st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
 with tab2:
    st.bar_chart(df_selection, x="mes", y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
-   test=px.bar(df_selection, x="mes", y="frequencia_incendios")
-   test.show()
+   
