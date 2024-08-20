70116a25a877766c53436f5c4d0c0ffa0a3f1c8a
Thiago Peres <180028278@aluno.unb.br>
2024-08-20 15:48:51 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -4,12 +4,14 @@ import leafmap.foliumap as leafmap
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide")
+st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide", initial_sidebar_state="collapsed")
 
 data = "DADOS/INPE_20_24.csv"
 adm = "DADOS/sdia_ra_2022.shp"
-area_protec = "DADOS/Area_de_Protecao_Ambiental.shp"
-
+dft= pd.read_csv('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/inmet_inpe.csv')
+anos = st.sidebar.selectbox("Selecione o ano:", dft["ano"].unique())
+df_selection = dft.query(
+        "ano == @anos")
 
 
 m = leafmap.Map(center=[-15.7, -47.7], zoom=10)
@@ -20,3 +22,5 @@ m.add_points_from_xy(df,
 m.split_map(left_layer='ROADMAP', right_layer='HYBRID')
 m.add_shp(adm)
 m.to_streamlit(height=800)
+st.bar_chart(dft, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
+st.bar_chart(df_selection, x="mes", y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
