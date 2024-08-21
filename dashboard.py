fc2d603962cdd76c286b5aaf007f3fe6885d1c52
Thiago Peres <180028278@aluno.unb.br>
2024-08-21 13:43:11 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -20,11 +20,9 @@ df_selection = df.query(
 
 with tab1:
     st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
+    fig = px.pie(df, values='frequencia_incendios', names='ano')
+    fig.show()
 
-    plost.donut_chart(
-        data=df,
-        use_container_width=True
-        )
 with tab2:
    st.bar_chart(df_selection, x="mes", y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
    
