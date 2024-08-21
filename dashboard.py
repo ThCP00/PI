ec961f6c77515f369d0d070e43a95db2ed13ff5c
Thiago Peres <180028278@aluno.unb.br>
2024-08-21 15:19:07 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -20,7 +20,7 @@ df_selection = df.query(
 
 with tab1:
     st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
-    st.bar_chart(df, x="ano" , y="PRECIPITACAO", color="rgb(255, 75, 75)")
+    st.bar_chart(df, x="ano" , y="PRECIPITACAO", color="rgb(255, 75, 75)", stack='layered')
 
 with tab2:
    st.bar_chart(df_selection, x="mes", y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
