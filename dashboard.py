8a6c36907f63c70222d154734c5f4735b78e2250
Thiago Peres <180028278@aluno.unb.br>
2024-08-21 15:33:33 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,7 +19,7 @@ df_selection = df.query(
         "ano == @anos")
 
 with tab1:
-    st.markdown("<h2 style='text-align: center; color: white;'>Frequencia de Incêndios por Ano</h2>",
+    st.markdown("<h3 style='text-align: center; color: white;'>Frequencia de Incêndios por Ano</h3>",
             unsafe_allow_html=True)
     st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
     st.bar_chart(df, x="ano" , y="PRECIPITACAO",x_label='Ano', y_label='Precipitação', stack='layered')
