2e11cc65d52419d823197c024de6c6e832e82113
Thiago Peres <180028278@aluno.unb.br>
2024-08-21 16:24:35 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -27,7 +27,7 @@ with tab1:
     st.bar_chart(df, x="ano" , y="PRECIPITACAO",x_label='Ano', y_label='Precipitação', stack='layered')
     st.markdown("<h3 style='text-align: center; color: white;'>Temperatura Média por Ano</h3>",
             unsafe_allow_html=True)
-    st.bar_chart(df, x="ano" , y="TEMPERATURA_MEDIA",x_label='Ano', y_label='Temperatura Média', stack='layered')
+    st.bar_chart(df, x="ano" , y="TEMPERATURA_MEDIA",x_label='Ano', y_label='Temperatura Média', color="rgb(255, 75, 75)", stack='layered')
 
 with tab2:
    st.markdown("<h3 style='text-align: center; color: white;'>Frequência de Incêndios por Mês</h3>",
@@ -38,4 +38,4 @@ with tab2:
    st.bar_chart(df_selection, x="mes_numero" , y="PRECIPITACAO", x_label='Mês', y_label='Precipitação',stack='layered')
    st.markdown("<h3 style='text-align: center; color: white;'>Temperatura Média por Mês</h3>",
             unsafe_allow_html=True)
-   st.bar_chart(df_selection, x="mes_numero" , y="TEMPERATURA_MEDIA", x_label='Mês', y_label='Temperatura Média',stack='layered')
+   st.bar_chart(df_selection, x="mes_numero" , y="TEMPERATURA_MEDIA", x_label='Mês', y_label='Temperatura Média', color="rgb(255, 75, 75)",stack='layered')
