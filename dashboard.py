f7db388b45597dc8aacde3910c746a015daaf2d7
Thiago Peres <180028278@aluno.unb.br>
2024-08-22 12:17:02 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import geopandas as gpd
-import altair as alt
-import plost
+
 
 st.set_page_config(
     page_icon='📈',
@@ -22,12 +21,8 @@ with tab1:
     st.markdown("<h3 style='text-align: center; color: white;'>Frequência de Incêndios por Ano</h3>",
             unsafe_allow_html=True)
     st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
-    st.markdown("<h3 style='text-align: center; color: white;'>Precipitação por Ano</h3>",
-            unsafe_allow_html=True)
-    st.bar_chart(df, x="ano" , y="PRECIPITACAO",x_label='Ano', y_label='Precipitação', stack='layered')
-    st.markdown("<h3 style='text-align: center; color: white;'>Temperatura Média por Ano</h3>",
-            unsafe_allow_html=True)
-    st.bar_chart(df, x="ano" , y="TEMPERATURA_MEDIA",x_label='Ano', y_label='Temperatura Média', color="rgb(255, 75, 75)", stack='layered')
+    fig=px.bar(df, x='ano', y='frequencia_incendios')
+    st.plotly_chart(fig, use_container_width=True)
 
 with tab2:
    st.markdown("<h3 style='text-align: center; color: white;'>Frequência de Incêndios por Mês</h3>",
