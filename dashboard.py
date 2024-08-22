cb22d29e5460149b3a32a141d37f270c9c929861
Thiago Peres <180028278@aluno.unb.br>
2024-08-22 12:34:42 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -11,13 +11,13 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 tab1, tab2 = st.tabs(["Ano","Mes"])
-c1, c2 = st.columns((5,5))
 df= pd.read_csv('https://raw.githubusercontent.com/ThCP00/PI/main/DADOS/inmet_inpe.csv')
 anos = st.sidebar.selectbox("Selecione o ano:", df["ano"].unique())
 df_selection = df.query(
         "ano == @anos")
 
 with tab1:
+    c1, c2 = st.columns((5,5))
     with c1:
         fig=px.pie(df, values='frequencia_incendios', names='ano')
         st.plotly_chart(fig, use_container_width=True,theme="streamlit")
@@ -29,6 +29,7 @@ with tab1:
     st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
 
 with tab2:
+    c1, c2 = st.columns((5,5))
    st.markdown("<h3 style='text-align: center; color: white;'>Frequência de Incêndios por Mês</h3>",
             unsafe_allow_html=True)
    st.bar_chart(df_selection, x="mes_numero", y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
