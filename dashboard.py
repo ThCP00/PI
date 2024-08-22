5b819fed653cd143b9edf3b414913312ef4eb463
Thiago Peres <180028278@aluno.unb.br>
2024-08-22 12:42:39 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -24,7 +24,8 @@ with tab1:
         fig=px.pie(df, values='frequencia_incendios', names='ano')
         st.plotly_chart(fig, use_container_width=True,theme="streamlit")
     with c2:
-        fig=px.scatter(df_selection, x="ano", y='frequencia_incendios',)
+        fig=px.scatter(df_selection, x="ano", y='frequencia_incendios')
+        fig.update_layout(scattermode="group", scattergap=0.75)
         st.plotly_chart(fig, use_container_width=True,theme="streamlit")
 
 with tab2:
