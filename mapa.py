ff415245e4add1049b87c559f2683654c5e98eb4
Thiago Peres <180028278@aluno.unb.br>
2024-08-22 15:01:35 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import leafmap.foliumap as leafmap
 import geopandas as gpd
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.set_page_config(page_icon='🔥', page_title='Calango', layout="wide", initial_sidebar_state="collapsed")
 tab1, tab2 = st.tabs(["Pontos","Mapa de Calor"])
