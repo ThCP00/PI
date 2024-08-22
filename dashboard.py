e5e355016d36cf035abf1abe859477190af88bd5
Thiago Peres <180028278@aluno.unb.br>
2024-08-22 12:36:15 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,6 +17,8 @@ df_selection = df.query(
         "ano == @anos")
 
 with tab1:
+    st.markdown("<h3 style='text-align: center; color: white;'>Frequência de Incêndios por Ano</h3>",unsafe_allow_html=True)
+    st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
     c1, c2 = st.columns((5,5))
     with c1:
         fig=px.pie(df, values='frequencia_incendios', names='ano')
@@ -24,13 +26,11 @@ with tab1:
     with c2:
         fig=px.scatter(df_selection, x="ano", y='frequencia_incendios')
         st.plotly_chart(fig, use_container_width=True,theme="streamlit")
-    st.markdown("<h3 style='text-align: center; color: white;'>Frequência de Incêndios por Ano</h3>",unsafe_allow_html=True)
-    st.bar_chart(df, x='ano', y='frequencia_incendios', x_label='Ano', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
 
 with tab2:
-   c1, c2 = st.columns((5,5))
    st.markdown("<h3 style='text-align: center; color: white;'>Frequência de Incêndios por Mês</h3>",unsafe_allow_html=True)
    st.bar_chart(df_selection, x="mes_numero", y='frequencia_incendios', x_label='Mês', y_label='Incêndios', color="rgb(255, 75, 75)", horizontal=False, stack='layered')
+   c1, c2 = st.columns((5,5))
    with c1:    
        fig=px.pie(df_selection, values='frequencia_incendios', names='mes')
        st.plotly_chart(fig, use_container_width=True,theme="streamlit")
