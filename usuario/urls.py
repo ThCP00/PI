d9785337a47a6be78fe11bdae062cc4ce641d84d
Thiago Peres <180028278@aluno.unb.br>
2024-08-23 10:49:31 -0300

    Add files via upload

--- usuario/urls.py
+++ usuario/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('cadastrar/', views.cadastrar, name='cadastrar'),
     path('realizar_login/', views.realizar_login, name='realizar_login'),
     path('sair/', views.sair, name='sair'),
+    path('mapa/', views.mapa, name='mapa'),
     path("", views.home, name="home"),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -34,5 +34,8 @@ def sair(request):
 def home(request):
     return render(request, "home.html")
 
+def mapa(request):
+    return render(request, "mapa.html")
+
 
 
